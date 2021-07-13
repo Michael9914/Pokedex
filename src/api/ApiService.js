72e4a822021735b6779c43683afe7f4f87bff6c3
Michael9914 <michael_9914@hotmail.com>
2021-07-12 19:03:46 -0500

    Pokedex

---
+++ src/api/ApiService.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { handleError, handleResponse } from "./ApiUtils";
+
+const httpRequest = (method, url, request, headers) => {
+  return axios({
+    method,
+    url,
+    data: request,
+    headers,
+  })
+    .then((res) => {
+      const result = handleResponse(res);
+      return Promise.resolve(result);
+    })
+    .catch((err) => {
+      return Promise.reject(handleError(err));
+    });
+};
+
+const get = (url, request, headers) => {
+  let queryString = "";
+  if (request && Object.keys(request).length > 0) {
+    queryString += "?";
+    let len = Object.keys(request).length,
+      cnt = 0;
+    for (let key in request) {
+      cnt++;
+      queryString += `${key}=${request[key].toString()}`;
+      if (len > cnt) queryString += "&";
+    }
+  }
+  return httpRequest("get", `${url}${queryString}`, request, headers);
+};
+
+
+const Api = {
+  get,
+
+};
+
+export default Api;
